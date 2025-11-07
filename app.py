d463d88e67cbc933fbe44489d66df4ad5393d954
SrikanthAwsDev <myfinops@gmail.com>
2025-11-07 18:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ with st.sidebar:
     savings_threshold = st.slider(
         "Minimum Savings Threshold ($)",
         min_value=0,
-        max_value=500,
+        max_value=2000,
         value=10,
         step=10
     )
